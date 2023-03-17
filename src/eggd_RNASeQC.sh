5c55c1c9382da5f0e4d3c213987d22e97150c0c0
AishaD <AishaDahir75@gmail.com>
2023-03-17 15:10:18 +0000

    PCF-81 make coverage optional

--- src/eggd_RNASeQC.sh
+++ src/eggd_RNASeQC.sh
@@ -1,45 +1,35 @@
 set -exo pipefail
 
 main() {
+
+    echo "-------------- Download files and set up -----------------"
     # Download all inputs in parallel
     dx-download-all-inputs
+    #Move all file paths to current directory
+	find ~/in -type f -name "*" -print0 | xargs -0 -I {} mv {} ./
 
     # Load docker image
-    docker load -i "${docker_name}"
+    docker=${docker_path##*/}
+    docker load -i "${docker}"
 
     # Get image id from docker image loaded
-    IMAGE_ID=$(sudo docker images --format="{{.Repository}} {{.ID}}" | grep "^ghcr" | cut -d' ' -f2)
+    IMAGE_ID=$(sudo docker images --format="{{.Repository}} {{.ID}}" | grep "^gcr" | cut -d' ' -f2)
 
+    echo "-------------- Running RNASeQC -----------------"
+    mkdir -p out/rnaseqc_out
+    gtf=${gtf_path##*/}
+	bam=${bam_path##*/}
+	bed=${bed_path##*/}
     # Run RNASeQ command depending on whether coverage is set
     if [ "$coverage" == 'true' ]; then
         docker run  -v /home/dnanexus/:/data ${IMAGE_ID} /bin/bash -c \
-        "cd /data ; rnaseqc $gtf $bam --bed $bed --coverage ." ;
-    else;
+        "cd /data ; rnaseqc $gtf $bam --bed $bed --coverage out/rnaseqc_out"
+    else
         docker run  -v /home/dnanexus/:/data ${IMAGE_ID} /bin/bash -c \
-        "cd /data ; rnaseqc $gtf $bam --bed $bed  ."
+        "cd /data ; rnaseqc $gtf $bam --bed $bed out/rnaseqc_out"
     fi
 
     echo "--------------Outputting files -----------------"
-    mkdir -p /home/dnanexus/out/exon_cv/
-    mkdir -p /home/dnanexus/out/exon_reads/
-    mkdir -p /home/dnanexus/out/gene_fragments/
-    mkdir -p /home/dnanexus/out/gene_reads/
-    mkdir -p /home/dnanexus/out/gene_tpm/
-    mkdir -p /home/dnanexus/out/metrics/
-
-    mv *exon_cv.tsv /home/dnanexus/out/exon_cv/
-    mv *exon_reads.gct /home/dnanexus/out/exon_reads/
-    mv *gene_fragments.gct /home/dnanexus/out/gene_fragments/
-    mv *gene_reads.gct /home/dnanexus/out/gene_reads/
-    mv *gene_tpm.gct /home/dnanexus/out/gene_tpm/
-    mv *metrics.tsv /home/dnanexus/out/metrics/
-
-    # If coverage was selected we need to make the folder and direct the
-    # the output file to it
-    if [ "$coverage" == 'true' ]; then
-        mkdir -p /home/dnanexus/out/coverage_tsv/
-        mv *coverage.tsv /home/dnanexus/out/coverage_tsv/
-    fi
 
     dx-upload-all-outputs
 
