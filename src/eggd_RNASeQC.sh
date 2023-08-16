a603250c93106b2b49bb87e451168b1072232695
AishaD <AishaDahir75@gmail.com>
2023-08-16 11:24:18 +0100

    PCF-129 unpack ctat

--- src/eggd_RNASeQC.sh
+++ src/eggd_RNASeQC.sh
@@ -18,14 +18,21 @@ main() {
     # Get image id from docker image loaded
     IMAGE_ID=$(sudo docker images --format="{{.Repository}} {{.ID}}" | grep "^gcr" | cut -d' ' -f2)
 
+    echo "---------- Decompress CTAT bundle -------------"
+    tar zxvf $CTAT_bundle_path
+    lib_dir=$(echo $CTAT_bundle_name | cut -d "." -f 1)
+
+    echo "------------- Collapse gtf file ----------------"
+    ref_annot_gtf="/home/dnanexus/genome_lib/${lib_dir}/ctat_genome_lib_build_dir/ref_annot.gtf"
+    python3 collapse_annotation.py $ref_annot_gtf ref_annot_collapse.gtf
+
     echo "-------------- Running RNASeQC -----------------"
     mkdir -p out/rnaseqc_out
-    gtf=${gtf_path##*/}
     bam=${bam_path##*/}
     bed=${bed_path##*/}
     # Run RNASeQ command depending on whether coverage is set
     docker_cmd="docker run  -v /home/dnanexus/:/data ${IMAGE_ID} /bin/bash -c \
-        'cd /data ; rnaseqc $gtf $bam --bed $bed "
+        'cd /data ; rnaseqc ref_annot_collapse.gtf $bam --bed $bed "
     if [ "$coverage" == 'true' ]; then
         docker_cmd+=" --coverage"
     fi
